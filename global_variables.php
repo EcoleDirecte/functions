9f1b1a953381584f3c907dfbbf1263936263756a
carloyt <carloyt@free.fr>
2016-12-24 15:50:59 +0100

    Add files via upload

---
+++ global_variables.php
@@ -0,0 +1,22 @@
+<?php
+
+DEFINE('FREEMB_SMSAPI_USER' , 'your_free_mobile_smsapi_user');
+DEFINE('FREEMB_SMSAPI_PASS' , 'your_free_mobile_smsapi_pass');
+DEFINE('FREEMB_SMSAPI_URL' , 'https://smsapi.free-mobile.fr/sendmsg?user=' . FREEMB_SMSAPI_USER . '&pass=' . FREEMB_SMSAPI_PASS . '&msg=');
+
+DEFINE('DB_HOST' , 'sql.example.com');
+DEFINE('DB_USER' , 'your_mysql_login');
+DEFINE('DB_PASS' , 'your_mysql_pass');
+DEFINE('DB_BDD' , 'your_mysql_DB');
+
+DEFINE('FTP_BIDOUI_SRV', 'ftp.bidouillages.tk');
+DEFINE('FTP_BIDOUI_USER', 'u769009388');
+DEFINE('FTP_BIDOUI_PASS', '');
+
+DEFINE('EC_CHARLES_LOGIN' , 'your_default_ecole_directe_login');
+DEFINE('EC_CHARLES_PASS' , 'your_default_ecole_directe_pass');
+
+// DEFINE('DB_LOGIN' , '');
+// DEFINE('DB_PASS' , '');
+
+?>
